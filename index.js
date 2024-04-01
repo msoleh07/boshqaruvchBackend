0a7face2a75b42a695978d01533c3544c0ddeda3
admin <admin@admins-MacBook-Air.local>
2024-04-01 11:52:12 +0500

    add node cron package

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 require("dotenv").config();
 require("colors");
 
+const nodeCron = require("node-cron");
+
 const PORT = process.env.PORT || 5500;
 
 const app = express();
@@ -12,6 +14,34 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => res.send(`<h1>Welcome to work server</h1>`));
+
+app.use("/api/butcheryUser", function (req, res, next) {
+  // node-cron ishga tushirish
+  nodeCron.schedule("* * * * * *", function () {});
+  // Keyingi middleware ga o'tish
+  next();
+});
+
+app.use("/api/workerUser", function (req, res, next) {
+  // node-cron ishga tushirish
+  nodeCron.schedule("* * * * * *", function () {});
+  // Keyingi middleware ga o'tish
+  next();
+});
+
+app.use("/api/owner", function (req, res, next) {
+  // node-cron ishga tushirish
+  nodeCron.schedule("* * * * * *", function () {});
+  // Keyingi middleware ga o'tish
+  next();
+});
+
+app.use("/api/prices", function (req, res, next) {
+  // node-cron ishga tushirish
+  nodeCron.schedule("* * * * * *", function () {});
+  // Keyingi middleware ga o'tish
+  next();
+});
 
 const butcheryUserRoutes = require("./routes/butcheryUser.routes"); // <-- 'butcheryUserRoutes' nomi to'g'rilandi
 const workerUserRoutes = require("./routes/workerUser.routes"); // <-- 'workerUserRoutes' nomi to'g'rilandi
